3e63787f0743e8ed9daa72327094e354cc9d5617
sikendrakumarverma <grdskverma2018@gmail.com>
2023-02-18 09:41:43 +0530

    form data manupulation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
+import Users from "./ui/Users";
+import { useEffect } from "react";
+
+//import axios from "axios";
+import { useState } from "react";
+import UserForm from "./ui/UserForm";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const [selectedUser, setSelectedUser] = useState({});
+
+  useEffect(() => {
+    console.log("Selcted User Changed..");
+  }, [selectedUser]);
+
+  useEffect(() => {
+    // console.log("App Mounted"); // On Component Mount
+    // axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    //   setUsers(response.data);
+    // });
+
+    return () => {};
+  }, []);
+
+  const [activePage, setActivePage] = useState(3);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+    setActivePage(2);
+  };
+
+  const handleOnDelete = (user) => {
+    console.log("delete ", user.id);
+
+    const indexToDelete = users.findIndex((item) => {
+      return item.id === user.id;
+    });
+
+    const updated = [...users];
+    updated.splice(indexToDelete, 1);
+
+    setUsers(updated);
+  };
+
+  const handleOnCreate = (user) => {
+    setUsers([user, ...users]);
+  };
+
+  //console.log({ users });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex"}}>
+      <div>
+        <button onClick={() => setActivePage(1)}>List of all users</button>
+        <button onClick={() => setActivePage(2)}>Details</button>
+        <button onClick={() => setActivePage(3)}>Form</button>
+      </div>
+
+      {activePage == 1 && (
+        <div >
+          {users.length > 0 ? (
+            <Users
+              onDelete={handleOnDelete}
+              onUserClick={handleUserClick}
+              users={users}
+            />
+          ) : (
+            <h1>No Users</h1>
+          )}
+        </div>
+      )}
+      {activePage == 2 && (
+        <div>
+          <h1>User Details</h1>
+          <hr />
+          <h1>
+            <pre>{JSON.stringify(selectedUser, null, 3)}</pre>
+          </h1>
+        </div>
+      )}
+      {activePage == 3 && (
+        <div className="form-wrapper">
+          <hr />
+          <UserForm onCreate={handleOnCreate} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/ui/UserForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+export default function UserForm(props) {
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = formValues.firstName + " " + formValues.lastName;
+
+    const user = {
+      ...formValues,
+      id: crypto.randomUUID(),
+      name: name,
+    };
+
+    props.onCreate(user);
+    alert("User crated Successfully Please click list button to show users list ")
+    
+        // setFormValues({
+        //     ...formValues,
+        //     firstName: '',
+        //     email: '',
+        //     lastName: ''
+        // })
+   
+  };
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <input
+        placeholder="First Name"
+        type="text"
+        onChange={handleOnChange}
+        name="firstName"
+        id=""
+      />
+      <input
+        placeholder="Last Name"
+        type="text"
+        onChange={handleOnChange}
+        name="lastName"
+        id=""
+      />
+      <input
+        placeholder="Email"
+        type="email"
+        onChange={handleOnChange}
+        name="email"
+        id=""
+      />
+      <input
+        placeholder="Phone"
+        type="number"
+        onChange={handleOnChange}
+        name="phone"
+        id=""
+      />
+      <input type="submit" value={"Create New User"} />
+    </form>
+  );
+}
